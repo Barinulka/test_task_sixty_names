b2f1ef1c9f93d50fce0242f1e8b1b8e7d957b895
Barinulka <barinulka@yandex.ru>
2023-04-20 12:29:44 +0400

    init

---
+++ MindBox/FigureLibrary/ClassLibrary/Figures/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Figures
+{
+    public class Circle : Lib
+    {
+
+        public double r { get; set; }
+
+        public Circle()
+        {
+
+            Console.WriteLine("Введите значение радиуса: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            if (radius < 0)
+            {
+                radius = 0;
+            }
+
+            r = radius;
+        }
+
+       
+        public override double Area()
+        {
+            double result;
+
+            result = Math.Round(Math.PI * (r * r), 2);
+
+            return result;
+        }
+
+    }
+}
---
+++ MindBox/FigureLibrary/ConsoleApp/Library.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Library
+    {
+        private Lib? _lib;
+
+        public void setFigure(Lib figure)
+        {
+            _lib = figure;
+        }
+
+        public double Area()
+        {
+            return _lib.Area();
+        }
+
+    }
+}
